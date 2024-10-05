2f9261a708ab4910ea3d57d07582a8760134a9b8
AliBaktash1 <ali1baktash2@gmail.com>
2024-10-05 09:47:01 -0700

    change the size of footer p5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/Logo.png';
 
 const Footer = () => {
   return (
-    <footer className='mt-[18px]'>
+    <footer className='mt-[20px]'>
     <hr />
     <p className='text-center p-1'> <img src={logo} alt="" className='inline-block w-6 h-6 ml-4' />ساخته شده در <span><a href="" className="text-blue-600">استارکوچ</a></span></p>
   </footer>
