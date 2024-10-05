93f99bab32165cc7547f8d9bf14f9ef492db41a1
AliBaktash1 <ali1baktash2@gmail.com>
2024-10-05 09:50:47 -0700

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/Logo.png';
 
 const Footer = () => {
   return (
-    <footer className='mt-[20px]'>
+    <footer className='mt-5'>
     <hr />
-    <p className='text-center p-1'> <img src={logo} alt="" className='inline-block w-6 h-6 ml-4' />ساخته شده در <span><a href="" className="text-blue-600">استارکوچ</a></span></p>
+    <p className='text-center p-2'> <img src={logo} alt="" className='inline-block w-6 h-6 ml-4' />ساخته شده در <span><a href="" className="text-blue-600">استارکوچ</a></span></p>
   </footer>
   );
 };
