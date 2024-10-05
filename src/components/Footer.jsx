3b24511e11fa8ff713979fa42bea8219e8b71184
AliBaktash1 <ali1baktash2@gmail.com>
2024-10-05 09:37:04 -0700

    change the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/Logo.png';
 
 const Footer = () => {
   return (
-    <footer className='mt-3'>
+    <footer className='mt-5'>
     <hr className="" />
     <p className='text-center p-1'> <img src={logo} alt="" className='inline-block w-6 h-6 ml-4' />ساخته شده در <span><a href="" className="text-blue-600">استارکوچ</a></span></p>
   </footer>
